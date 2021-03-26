6e0beb8c97e0733fda1ec15521b75ca93c5c114a
engrreis <engrreis@gmail.com>
2021-03-25 21:47:24 -0300

    inicio

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home(){
+    return <div> 
+        <h1>
+            Catarina essa é nossa primera página chama o heitor pra ver
+        </h1>
+        <p><h1><font color="red">GIOVANA, HEITOR E MAMAE - PAPAI AMA VOCES</font></h1></p>
+        </div>
+}
+
+export default Home
